061037c9ff99c0c3b6b8a87220604691cbe96ea2
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-08-14 02:14:05 +0700

    config: tailwindcss & route

---
+++ e-wallet-admin/src/components/sidebar/sidebar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBarPart from './sidebar_part'
+
+const SideBar = () => {
+  return (
+    <div className='max-w-[250px] w-full'>
+        <SideBarPart></SideBarPart>
+    </div>
+  )
+}
+
+export default SideBar
---
+++ e-wallet-admin/src/components/sidebar/sidebar_part.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBarPart = () => {
+  return (
+    <div className='h-[50px] w-full'>
+        <div className='h-full w-[20px] rounded-l-lg bg-blue-600'>1</div>
+    </div>
+  )
+}
+
+export default SideBarPart
