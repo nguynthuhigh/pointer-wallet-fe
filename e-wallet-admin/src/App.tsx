0c8db455dfc53679324d9758bc06a90b24a25b25
NguynThuHigh <minhnguyen11a1cmg@gmail.com>
2024-08-14 02:51:59 +0700

    style: sidebar

--- e-wallet-admin/src/App.tsx
+++ e-wallet-admin/src/App.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route,Routes } from "react-router-dom"
-import Home from "./pages/home"
+import Home from "./pages/test"
+import DashBoard from "./pages/dashboard"
 function App() {
 
   return (
     <BrowserRouter>
        <Routes>
         <Route path="/test" element={<Home></Home>}/>
+        <Route path="/dashboard" element={<DashBoard></DashBoard>}/>
       </Routes>
     </BrowserRouter>
  
--- e-wallet-admin/src/components/sidebar/sidebar.tsx
+++ e-wallet-admin/src/components/sidebar/sidebar.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SideBarPart from './sidebar_part'
-
-const SideBar = () => {
-  return (
-    <div className='max-w-[250px] w-full'>
-        <SideBarPart></SideBarPart>
+import UserIcon from '../../assets/svg/user.svg'
+const SideBar = ({...props}) => {
+    const [selected,setSelected] = useState<string>(props.state)
+    const handleSelect = (state:string)=>{
+        setSelected(state)
+    }
+    return (
+    <div className='max-w-[220px] w-full'>
+        <SideBarPart link='/test' selected={selected} handleSelect = {()=>{handleSelect('Manage User')}} name="Manage User" icon={UserIcon}></SideBarPart>
+        <SideBarPart link='/dashboard' selected={selected} handleSelect = {()=>{handleSelect('Dashboard')}} name='Dashboard' icon={UserIcon}></SideBarPart>
     </div>
   )
 }
