71a909865272d516ca5526a408759d7a14f72985
Luong Tu Nhan <tunhan281204@gmail.com>
2024-08-21 21:16:06 +0700

    test

--- e-wallet-app/src/pages/deposit-withdraw/index.tsx
+++ e-wallet-app/src/pages/deposit-withdraw/index.tsx
@@ -4,6 +4,7 @@ export default function DepositWithdraw() {
   return (
     <div class={`p-4`}>
       <HeaderDefault title="Nạp / Rút" />
+      <div>Hello</div>
       <Outlet />
     </div>
   );
