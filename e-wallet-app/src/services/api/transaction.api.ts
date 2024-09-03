491a07eceb6f2e1bcb9740c19df77771e7801b96
Lương Tư Nhân <166272042+ltnhan04@users.noreply.github.com>
2024-09-03 23:10:05 +0700

    Update transaction.api.ts

--- e-wallet-app/src/services/api/transaction.api.ts
+++ e-wallet-app/src/services/api/transaction.api.ts
@@ -1,10 +1,10 @@
-import { createAxios } from "../../config/axios.config";
+import  { createAxios } from '../../config/axios.config'
 const axiosInstance = createAxios();
-export const getTransactionPaginate = async (page: number, limit: number) => {
-  return await axiosInstance.get(
-    `/api/v1/transaction/get/transactions?page=${page}&limit=${limit}`,
-    {
-      withCredentials: true,
-    }
-  );
-};
+export const getTransactionPaginate = async (page: number,limit:number) => {
+    return await axiosInstance.get(
+      `/api/v1/transaction/get/transactions?page=${page}&limit=${limit}`,
+      {
+        withCredentials:true
+      }
+    );
+  };
