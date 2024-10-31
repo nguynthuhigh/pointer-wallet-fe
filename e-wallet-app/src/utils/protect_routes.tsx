cbaa3808f53303671fadbf863382f9a64fcc2e6b
NguynThuHig <minhnguyen11a1>
2024-10-31 20:00:13 +0700

    fix: protected routes

--- e-wallet-app/src/utils/protect_routes.tsx
+++ e-wallet-app/src/utils/protect_routes.tsx
@@ -1,8 +1,16 @@
 import { Outlet, Navigate } from "react-router-dom";
+import { getCookie } from "./cookie";
+import { jwtDecode, JwtPayload } from "jwt-decode";
 
-const ProtectRoutes = () => {
-  const logged = localStorage.getItem("logged");
-  return logged ? <Outlet /> : <Navigate to="/auth/login" />;
+const ProtectRoutes = async () => {
+  const token = await getCookie("at");
+  if (!token) {
+    return <Navigate to="/auth/login" />;
+  }
+  const decoded = jwtDecode<JwtPayload>(token);
+  const currentTime = Math.floor(Date.now() / 1000);
+  const isTokenExpired = decoded.exp ? decoded.exp < currentTime : true;
+  return isTokenExpired ? <Navigate to="/auth/login" /> : <Outlet />;
 };
 
 export default ProtectRoutes;
